706071281dab5da9deb36f88ecb4ee0a63a1173d
Mr.Bakai <ismailov.time@gmail.com>
2024-03-10 17:53:34 +0600

    LC: Clean up

--- LeetCode/Easy/35. Search Insert Position.swift
+++ LeetCode/Easy/35. Search Insert Position.swift
@@ -29,8 +29,6 @@ func searchInsert(
     return low
 }
 
-
 //                             👇target
 // [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 //  👆🏻low           👆🏻guess                   👆🏻high
-
