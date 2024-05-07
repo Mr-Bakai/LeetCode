3e50c8c76b0255916133a5be8459d30c4914abe3
Mr.Bakai <ismailov.time@gmail.com>
2024-05-07 21:18:08 +0600

    242. Valid Anagram is done

---
+++ LeetCode/Easy/Set/242. Valid Anagram.swift
@@ -0,0 +1,28 @@
+//
+//  242. Valid Anagram.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 7/5/24.
+//
+
+import Foundation
+
+func isAnagram(
+    _ s: String = "aacc",
+    _ t: String = "ccac"
+) -> Bool {
+    guard s.count == t.count else { return false }
+    
+    var dictS = [Character:Int]()
+    var dictT = [Character:Int]()
+    
+    for i in s {
+        dictS[i, default: 0] += 1
+    }
+    
+    for i in t {
+        dictT[i, default: 0] += 1
+    }
+    
+    return dictS == dictT
+}
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -85,3 +85,6 @@ print(
 print(
     "containsDuplicate: \(containsDuplicate())"
 )
+print(
+    "isAnagram: \(isAnagram())"
+)
