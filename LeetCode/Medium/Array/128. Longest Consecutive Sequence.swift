744c992b07e8e2c58fe8bc21416d2985f7570980
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-07-18 17:43:00 +0600

    isValidSudoku is started

--- LeetCode/Medium/Array/128. Longest Consecutive Sequence.swift
+++ LeetCode/Medium/Array/128. Longest Consecutive Sequence.swift
@@ -25,14 +25,12 @@
 func longestConsecutive(
     _ nums: [Int] = [100, 4, 200, 1, 3, 2]
 ) -> Int {
-    
     let numSet = Set(nums)
     var longest = 0
     
     for n in nums {
         if !numSet.contains((n - 1)) {
             var length = 0
-            
             while numSet.contains((n + length)) {
                 length += 1
             }
---
+++ LeetCode/Medium/Array/36. Valid Sudoku.swift
@@ -0,0 +1,13 @@
+//
+//  36. Valid Sudoku.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 18/7/24.
+//
+
+func isValidSudoku(
+    _ board: [[Character]]
+) -> Bool {
+    
+    return true
+}
