a02be64d7b65a173717286fbae6c6234e6c731cf
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-08-05 16:39:06 +0600

    Group Anagram Repetition is started

---
+++ Repeatition/Medium/HashMap/49. Group Anagrams Repeat.swift
@@ -0,0 +1,19 @@
+//
+//  49. Group Anagrams Repeat.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 5/8/24.
+//
+
+// Example 1:
+//
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+func groupAnagramsRepeat(
+    _ strs: [String] = ["eat","tea","tan","ate","nat","bat"]
+) -> [[String]] {
+    
+    var dict = [[Int]: [String]]()
+    
+    return [[""]]
+}
