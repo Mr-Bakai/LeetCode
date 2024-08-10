a750e586c5de73ece1b9fa46b3bb46439c82318b
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-08-10 14:38:12 +0600

    680. Valid Palindrome II is started

---
+++ LeetCode/Easy/TwoPointer/680. Valid Palindrome II.swift
@@ -0,0 +1,41 @@
+//
+//  680. Valid Palindrome II.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 10/8/24.
+//
+
+/// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+///
+/// Example 1:
+/// Input: s = "aba"
+/// Output: true
+///
+/// Example 2:
+/// Input: s = "abca"
+/// Output: true
+/// Explanation: You could delete the character 'c'.
+///
+///
+/// Example 3:
+/// Input: s = "abc"
+/// Output: false
+///
+/// Constraints:
+///
+/// 1 <= s.length <= 105
+/// s consists of lowercase English letters.
+
+func validPalindrome(
+    _ s: String
+) -> Bool {
+    
+    var left = 0
+    var right = s.count
+    
+    while left < right {
+        
+    }
+    
+    return true
+}
