b0408fd5060dc881a6459815ea353f6db5411512
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-08-29 08:33:19 +0600

    155. Min Stack is done

--- LeetCode/Medium/Stack/155. Min Stack.swift
+++ LeetCode/Medium/Stack/155. Min Stack.swift
@@ -5,4 +5,33 @@
 //  Created by Bakai Ismaiilov on 29/8/24.
 //
 
-import Foundation
+class MinStack {
+    private var stack: [Int]
+    private var minStack: [Int]
+    
+    init() {
+        stack = []
+        minStack = []
+    }
+    
+    func push(_ val: Int) {
+        stack.append(val)
+        let minVal = minStack.isEmpty ? val : min(val, minStack.last!)
+        minStack.append(minVal)
+    }
+    
+    func pop() {
+        if !stack.isEmpty {
+            stack.removeLast()
+            minStack.removeLast()
+        }
+    }
+    
+    func top() -> Int {
+        return stack.last ?? -1
+    }
+    
+    func getMin() -> Int {
+        return minStack.last ?? -1
+    }
+}
