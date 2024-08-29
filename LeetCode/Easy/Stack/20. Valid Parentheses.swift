df7fcf7165cccf64e03dd2afffbb6b0f6c0dca07
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-08-29 08:32:21 +0600

    minStack is in the process

--- LeetCode/Easy/Stack/20. Valid Parentheses.swift
+++ LeetCode/Easy/Stack/20. Valid Parentheses.swift
@@ -39,6 +39,46 @@
 /// 1 <= s.length <= 104
 /// s consists of parentheses only '()[]{}'.
 
-func isValid(_ s: String) -> Bool {
+func isValid(
+    _ s: String = "([])"
+) -> Bool {
     
+    var stack = [Character]()
+    
+    for char in s {
+        stack.append(char)
+    }
+    
+    return false
+}
+
+class MinStack {
+    private var stack: [Int]
+    private var minStack: [Int]
+    
+    init() {
+        stack = []
+        minStack = []
+    }
+    
+    func push(_ val: Int) {
+        stack.append(val)
+        let minVal = minStack.isEmpty ? val : min(val, minStack.last!)
+        minStack.append(minVal)
+    }
+    
+    func pop() {
+        if !stack.isEmpty {
+            stack.removeLast()
+            minStack.removeLast()
+        }
+    }
+    
+    func top() -> Int {
+        return stack.last ?? -1
+    }
+    
+    func getMin() -> Int {
+        return minStack.last ?? -1
+    }
 }
---
+++ LeetCode/Medium/Stack/155. Min Stack.swift
@@ -0,0 +1,8 @@
+//
+//  155. Min Stack.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 29/8/24.
+//
+
+import Foundation
