9265ff3d079bbad05d2060c20bea62f8e9b2630a
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-08-31 16:55:25 +0600

    22. Generate Parentheses is done

--- LeetCode/Medium/Stack/22. Generate Parentheses.swift
+++ LeetCode/Medium/Stack/22. Generate Parentheses.swift
@@ -25,5 +25,30 @@ func generateParenthesis(
     _ n: Int = 3
 ) -> [String] {
     
-    return [""]
+    var res = [String]()
+    var stack = [String]()
+    
+    func backtrack(
+        _ openN: Int,
+        _ closedN: Int
+    ) {
+        if openN == closedN && openN == n {
+            res.append(stack.joined())
+            return
+        }
+        
+        if openN < n {
+            stack.append("(")
+            backtrack(openN + 1, closedN)
+            stack.removeLast()
+        }
+        if closedN < openN {
+            stack.append(")")
+            backtrack(openN, closedN + 1)
+            stack.removeLast()
+        }
+    }
+    
+    backtrack(0, 0)
+    return res
 }
