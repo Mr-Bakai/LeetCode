d02f02cabcf5d6ba48f0d557c58bfe24115b6505
Bakai Ismaiilov <ismailov.time@gmail.com>
2024-09-23 07:57:34 +0600

    503. Next Greater Element II is done, brute force

--- LeetCode/Medium/Stack/503. Next Greater Element II.swift
+++ LeetCode/Medium/Stack/503. Next Greater Element II.swift
@@ -7,63 +7,62 @@
 
 /*
  
-Given a circular integer array nums (i.e., the next element of
-nums[nums.length - 1] is nums[0]),
-return the next greater number for every element in nums.
-
-The next greater number of a number x is the first greater number 
-to its traversing-order next in the array, which means you could 
-search circularly to find its next greater number.
-If it doesn't exist, return -1 for this number.
-
-
-Example 1:
-Input: nums = [1,2,1]
-Output: [2,-1,2]
+ Given a circular integer array nums (i.e., the next element of
+ nums[nums.length - 1] is nums[0]),
+ return the next greater number for every element in nums.
  
-Explanation: The first 1's next greater number is 2;
-The number 2 can't find next greater number.
-The second 1's next greater number needs to search circularly, 
-which is also 2.
+ The next greater number of a number x is the first greater number
+ to its traversing-order next in the array, which means you could
+ search circularly to find its next greater number.
+ If it doesn't exist, return -1 for this number.
  
-Example 2:
-Input: nums = [1,2,3,4,3]
-Output: [2,3,4,-1,4]
  
-
-Constraints:
-1 <= nums.length <= 104
--109 <= nums[i] <= 109
+ Example 1:
+ Input: nums = [1,2,1]
+ Output: [2,-1,2]
  
-*/
+ Explanation: The first 1's next greater number is 2;
+ The number 2 can't find next greater number.
+ The second 1's next greater number needs to search circularly,
+ which is also 2.
+ 
+ Example 2:
+ Input: nums = [1,2,3,4,3]
+ Output: [2,3,4,-1,4]
+ 
+ 
+ Constraints:
+ 1 <= nums.length <= 104
+ -109 <= nums[i] <= 109
+ 
+ */
 
 func nextGreaterElements(
-    _ nums: [Int] = [1,2,1]
+    _ nums: [Int] = [1,2,3,4,3]
 ) -> [Int] {
     
-    var mappedIndexes: [Int:Int] = [:]
-    var stack: [Int] = []
-    var res = [Int](
+    var stack = [Int]()
+    var res = Array(
         repeating: -1,
         count: nums.count
     )
     
-    for (index, value) in nums.enumerated() {
-        mappedIndexes[index] = value
-    }
-    
-    for cur in nums {
-        while let last = stack.last, cur > last {
-            stack.removeLast()
-            if let index = mappedIndexes[nums.firstIndex(of: last)!] {
-                res[index] = cur
+    /// Loop twice to simulate the circular nature of the array
+    for _ in 0..<2 {
+        for (i, e) in nums.enumerated() {
+            
+            /// While stack is not empty and current element is
+            /// greater than the element at the top of the stack
+            while !stack.isEmpty && e > nums[stack.last!] {
+                
+                /// Update the result array with the next greater element
+                res[stack.removeLast()] = e
             }
+            
+            /// Push the index of the current element onto the stack
+            stack.append(i)
         }
-        
-        stack.append(cur)
     }
     
-    print(" test: \(res)")
-    print(" mappedIndexes: \(mappedIndexes)")
     return res
 }
