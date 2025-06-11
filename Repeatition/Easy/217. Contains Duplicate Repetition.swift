897f2b0df34487925048ef56f95c348afe425cdf
Bakai <bakai.ismaiilov@optimabank.kg>
2025-06-11 21:45:33 +0600

    [LA]: containsDuplicateRepetition2 is done

--- Repeatition/Easy/217. Contains Duplicate Repetition.swift
+++ Repeatition/Easy/217. Contains Duplicate Repetition.swift
@@ -41,11 +41,30 @@ func containsDuplicateRepetition(
 ) -> Bool {
     var hashSet = Set<Int>()
     
+    print("containsDuplicateRepetition2() \(containsDuplicateRepetition2())")
+    
     for i in nums {
         if hashSet.contains(i) {
             return true
         }
         hashSet.insert(i)
+    }
+    return false
+}
+
+func containsDuplicateRepetition2(
+    _ nums: [Int] = [1,2,3,4]
+) -> Bool {
+    let sorted = nums.sorted()
+    var left = 0
+    var right = 1
+    
+    while right <= sorted.count - 1 {
+        if sorted[left] == sorted[right] {
+            return true
+        }
+        left += 1
+        right += 1
     }
     return false
 }
