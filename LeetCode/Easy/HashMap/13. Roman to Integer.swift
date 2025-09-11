6f8b55eb25ccaba9a7ff47ca9ceac3bc52be470a
Bakai <bakai.ismaiilov@optimabank.kg>
2025-09-11 11:20:03 +0900

    [LC]: 13. Roman to Integer better solution is done

--- LeetCode/Easy/HashMap/13. Roman to Integer.swift
+++ LeetCode/Easy/HashMap/13. Roman to Integer.swift
@@ -13,7 +13,6 @@
 /// C                 100
 /// D                 500
 /// M                 1000
-///
 
 
 /// I can be placed before V (5) and X (10) to make 4 and 9.
@@ -65,4 +64,61 @@ func romanToInt(_ s: String = "MCMXCIV") -> Int {
         }
     }
     return sum
+}
+
+
+func romanToInt2(_ s: String = "MCMXCIV") -> Int {
+    let s = Array(s)
+    
+    let romHashMap: [Character: Int] = [
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    ]
+    var total = 0
+    
+    // 1000 - 100 + 1000 - 10 + 100 - 1 + 5 = 1994
+    
+    ///  i = 0
+    ///  M       <   C    add branch +
+    ///  1000  <   100
+    ///
+    ///  i = 1
+    ///  C      <   M     sub branch -
+    ///  100   <   1000
+    ///
+    ///  i = 2
+    ///  M       <   X     add branch +
+    ///  1000  <   10
+    ///
+    ///  i = 3
+    ///  X      <   C      sub branch -
+    ///  10     <   100
+    ///
+    ///  i = 4
+    ///  C       <   I      add branch +
+    ///  100    <   1
+    ///
+    ///  i = 5
+    ///  I          <     V    sub branch -
+    ///  1          <     5
+    ///
+    /// adds last one
+    
+    for i in 0..<s.count - 1 {
+        let current = s[i]
+        let next = s[i+1]
+        
+        if romHashMap[current]! < romHashMap[next]! {
+            total -= romHashMap[current]!
+        } else {
+            total += romHashMap[current]!
+        }
+    }
+    total += romHashMap[s[s.count - 1]]!
+    return total
 }
