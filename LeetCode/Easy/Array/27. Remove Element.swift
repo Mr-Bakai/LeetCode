50ad870eebfa7cf0fc56c95c7e43c9c800f5d367
BAKAI <ismailov.time@gmail.com>
2025-09-14 13:05:10 +0900

    [LC]: 27. Remove Element is started

---
+++ LeetCode/Easy/Array/27. Remove Element.swift
@@ -0,0 +1,14 @@
+//
+//  27. Remove Element.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 14/9/25.
+//
+
+func removeElement(
+    _ nums: inout [Int],
+    _ val: Int
+) -> Int {
+    
+    return -1
+}
