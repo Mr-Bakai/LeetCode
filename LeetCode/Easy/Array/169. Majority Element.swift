ca099cd204e1b9cf9b231d60696ea41e1ca52d79
BAKAI <ismailov.time@gmail.com>
2025-10-03 22:51:10 +0900

    [LC]: 169. Majority Element is done

---
+++ LeetCode/Easy/Array/169. Majority Element.swift
@@ -0,0 +1,44 @@
+//
+//  169. Majority Element.swift
+//  LeetCode
+//
+//  Created by Bakai Ismaiilov on 3/10/25.
+//
+
+/// Given an array nums of size n, return the majority element.
+/// The majority element is the element that appears more than ⌊n / 2⌋ times.
+/// You may assume that the majority element always exists in the array.
+///
+/// Example 1:
+/// Input: nums = [3,2,3]
+/// Output: 3
+///
+/// Example 2:
+/// Input: nums = [2,2,1,1,1,2,2]
+/// Output: 2
+///
+/// Constraints:
+/// n == nums.length
+/// 1 <= n <= 5 * 104
+/// -109 <= nums[i] <= 109
+
+
+func majorityElement(
+    _ nums: [Int] = [3,2,3]
+) -> Int {
+    
+    let leftOver = nums.count / 2
+    var count = [Int: Int]()
+    
+    for num in nums {
+        count[num, default: 0] += 1
+    }
+    
+    for (key, val) in count {
+        if val > leftOver {
+            return key
+        }
+    }
+    
+    return -1
+}
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -219,3 +219,5 @@ var nums3 = [1,2,3,0,0,0]
 print("merge: \(merge(&nums3, 3, [2,5,6], 3))")
 
 print("singleNumber: \(singleNumber())")
+
+print("majorityElement: \(majorityElement())")
